d13a03a2db0cf69332e575a8caa4e6a8b20cbdc9
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-04-03 00:03:46 -1100

    Update SimpleSort.cpp

--- SimpleSort.cpp
+++ SimpleSort.cpp
@@ -2,7 +2,7 @@ void SimpleSort(std::vector<int>& v)
 {
     for (int i = 0; i < v.size(); i++)
    {
-        for (int j = 1; j < v.size(); j++)
+        for (int j = 0; j < v.size(); j++)
         {
             if (v[i] < v[j])
             {
