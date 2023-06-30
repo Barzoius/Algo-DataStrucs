7602bd749b4ff700373113cfd811e96f8f17d5c4
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-06-29 22:12:37 -1100

    Update Just_a_BinTree.cpp

--- DataStructures/Trees/BinaryTrees/Just_a_BinTree.cpp
+++ DataStructures/Trees/BinaryTrees/Just_a_BinTree.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+class BinNode{
+
+    int value;
+    BinNode* left_first;
+    BinNode* right_second;
+
+public: 
+    explicit BinNode(int val) :
+     value(val),
+     left_first(nullptr),
+     right_second(nullptr){}
+};
