c5fec59b905175fb90a3d47b5219c60b135b443a
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-07-02 07:23:06 -1100

    Update RMQ.cpp

--- DataStructures/RMQ/RMQ.cpp
+++ DataStructures/RMQ/RMQ.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 #include <cmath>
 
 class RMQ{
@@ -13,14 +14,15 @@ public:
     std::vector<std::vector<int>> CREATE_RMQ() const{
         std::vector<std::vector<int>> r(17, std::vector<int>(n));
         for(int i = 0; i < n; i++){
-            std::cin>>r[0][i];
+            r[0][i] = v[i];
         }
 
         for(int p = 1; (1 << p) <= n; p++){
             for(int i = 0; i < n; i++){
                 r[p][i] = r[p - 1][i];
-                int k = i + (int)pow(2, (p - 1));
-                if(k <= n && r[p][i] > r[p -1][i])
+//                int k = i + (int)pow(2, (p - 1)); both k work
+                int k = i + (1<<(p-1));
+                if(k <= n && r[p][i] > r[p -1][k])
                     r[p][i] = std::min(r[p - 1][i], r[p - 1][k]);
             }
         }
@@ -30,11 +32,21 @@ public:
     }
 
     static void DISPLAY(const std::vector<std::vector<int>>& r){
-        for(int p = 0; p < r.size(); p++ ){
-            std::cout<<"2^("<<p<<")"<<":\n";
+
+        int size = r[0].size();
+        int c = (int)pow(2, floor(log2(size)));
+        std::cout << std::left << std::setw(10) << "Pow of 2 ";
+        for(int p = 0; p < size; p++) {
+            std::cout<<std::left<<std::setw(10)<<p;
+        }
+
+        for(int p = 0; p < c; p++ ){
+            std::cout<<std::endl;
+            std::cout<<std::left<<std::setw(0)<<"2^("<<p<<"): "<<std::left<<std::setw(5);
             for(int i = 0; i < r[0].size(); i++){
-                std::cout<< r[p][i] << " ";
+                std::cout<<std::left<<std::setw(10)<<r[p][i];
             }
+            std::cout<<std::endl;
         }
     }
 };
@@ -42,9 +54,9 @@ public:
 int main() {
 
     int N ;
-    std::vector<int> V;
-
     std::cin>>N;
+    std::vector<int> V(N);
+
     for(int i = 0; i < N; i++){
         std::cin>>V[i];
     }
@@ -55,4 +67,3 @@ int main() {
 
     return 0;
 }
-
