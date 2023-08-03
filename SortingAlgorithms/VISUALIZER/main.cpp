f8848e87064559c18fffcffed6f5e7048ba34517
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-03 03:21:15 -1100

    Update main.cpp

--- SortingAlgorithms/VISUALIZER/main.cpp
+++ SortingAlgorithms/VISUALIZER/main.cpp
@@ -1,1 +1,15 @@
+#include "../HEADERS/VIS.hpp"
+
+ int main(int argc, char* argv[])
+ {
+    std::uniform_int_distribution<int> d(1, 99);
+    std::random_device random;
+    std::vector<int> vec;
+    for(int i = 0; i < 100; i++){
+        vec.push_back(d(random));
+    }
+     Visualizer V;
+     V.SimpleSort(vec);
+     return 0;
+ }
 
