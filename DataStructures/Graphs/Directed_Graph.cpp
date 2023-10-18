4e523a4f451b8f2b91c8591e2fe1b089ee7b85c9
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-10-18 01:50:02 -1100

    Update Directed_Graph.cpp

--- DataStructures/Graphs/Directed_Graph.cpp
+++ DataStructures/Graphs/Directed_Graph.cpp
@@ -4,23 +4,15 @@
 #include <iostream>
 #include <algorithm>
 
-struct Vertex{
-
-};
-
-struct Undirected_Edge{
-
-};
-
-struct Directed_Edge{
-
-};
-
 class Implementations{
 public:
     virtual void Add_to_adj_list(std::vector<int>&, int) = 0;
     virtual void Add_to_edge_list(std::tuple<int, int>&) = 0;
+
+    virtual ~Implementations() = default;
 };
+
+class iterator;
 
 class Graph : public virtual Implementations{
 
@@ -30,13 +22,16 @@ private:
     std::vector<std::vector<int>> Adjacency_List;
     std::vector<std::tuple<int, int>> Edges_List;
 
-public:
+    std::vector<bool> VIS;
 
+public:
+    std::vector<bool> GetVis() const { return VIS; }
     Graph() : num_vertices(0) {}
 
     explicit Graph(size_t vertices_number ) : num_vertices(vertices_number)
     {
         Adjacency_List.resize(vertices_number);
+        VIS.resize(vertices_number, false);
     }
 
     std::vector<std::vector<int>> Get_Adjacency_List(){ return Adjacency_List;}
@@ -60,16 +55,17 @@ public:
         Adjacency_List.resize(num_vertices + x);
     }
 
-    void PrintGraph(){
+    void PrintAdjList(){
 
         for(size_t i = 0; i < Adjacency_List.size(); i++){
-            std::cout<<i+1<<": ";
+            std::cout<<i+1<<": "<<"[";
             for(size_t j = 0; j < Adjacency_List[i].size() ; j++){
                 std::cout << Adjacency_List[i][j];
                 if (j < Adjacency_List[i].size() - 1) {
                     std::cout << ", ";
                 }
             }
+            std::cout<<"]";
             std::cout<<std::endl;
         }
     }
@@ -85,39 +81,81 @@ public:
             std::cout<<std::endl;
         }
     }
+
+
+    void DFS(int start_vertex, bool flag)
+    {
+        start_vertex--;
+        VIS[start_vertex] = true;
+
+        if(flag) std::cout << start_vertex + 1 << " ";
+
+
+        for(const auto&  j : Adjacency_List[start_vertex])
+        {
+            int adj_vertex = j - 1;
+
+            if(!VIS[adj_vertex])
+                DFS(j, flag);
+
+        }
+    }
+
+    void PrintDFS()
+    {
+        std::fill(VIS.begin(), VIS.end(), false);
+        std::cout<<"The DFS traversal is: ";
+        for (int i = 0; i < Get_Graph_Size(); i++)
+            if (!GetVis()[i])
+                DFS(i + 1, true);
+    }
+
+
+    int GetConexElemets()
+    {
+        std::fill(VIS.begin(), VIS.end(), false);
+        int cnt = 0;
+        for (int i = 0; i < Get_Graph_Size(); i++)
+        {
+            if (!GetVis()[i])
+            {
+                cnt++;
+                DFS(i + 1, false);
+            }
+
+        }
+        std::cout<<std::endl;
+        std::cout<<"The number of conex elemets is: " << cnt;
+
+        return cnt;
+    }
 };
 
-class Undirected_Graph : public Graph{
-
-};
-
-class Directed_Graph : public Graph{
-
-};
 
 int main() {
 
-    Graph g(2);
-    std::vector<int> vec1 = {12, 32, 43, 43};
-    std::vector<int> vec2 = {52, 2, 43, 3567};
-    g.Add_to_adj_list(vec1, 0);
-    g.PrintGraph();
-    g.Add_Vertex(3);
-    g.Add_to_adj_list(vec2, 2);
-    g.PrintGraph();
+    Graph g(7);
+    std::vector<int> vec1 = {2};
+    std::vector<int> vec2 = {1, 6, 7};
+    std::vector<int> vec3 = {};
+    std::vector<int> vec4 = {5};
+    std::vector<int> vec5 = {4};
+    std::vector<int> vec6 = {2};
+    std::vector<int> vec7 = {2};
 
-    Graph h;
-    std::tuple<int, int> tup1 = std::make_tuple(1, 2);
-    std::tuple<int, int> tup4 = std::make_pair(3, 2);
-    std::tuple<int, int> tup2 = std::make_tuple(2, 1);
-    std::tuple<int, int> tup3 = std::make_tuple(1, 3);
-    h.Add_to_edge_list(tup1);
-    h.Add_to_edge_list(tup4);
-    h.Add_to_edge_list(tup2);
-    h.Add_to_edge_list(tup3);
-    h.PrintEdges();
+    g.Add_to_adj_list(vec1, 0);
+    g.Add_to_adj_list(vec2, 1);
+    g.Add_to_adj_list(vec3, 2);
+    g.Add_to_adj_list(vec4, 3);
+    g.Add_to_adj_list(vec5, 4);
+    g.Add_to_adj_list(vec6, 5);
+    g.Add_to_adj_list(vec7, 6);
+    g.PrintAdjList();
+
+    g.PrintDFS();
+    g.GetConexElemets();
+    std::cout<<std::endl;
 
 
     return 0;
 }
-
