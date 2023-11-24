847eb2ba5e06cb51b546c8215f26a64a2d6e2fea
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-11-24 01:00:17 -1100

    Update Kruskal_1.cpp

--- Algorithms/Graphs_Algorithms/KRUSKAL/Kruskal_1.cpp
+++ Algorithms/Graphs_Algorithms/KRUSKAL/Kruskal_1.cpp
@@ -1,1 +1,34 @@
-std::vector<int> distance;
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+class DSU{
+
+private:
+    std::vector<int> parent;
+    std::vector<int> rank;
+
+public:
+
+    DSU(int n) : parent(n, -1), rank(n, 1) {}
+
+    int DSU_find(int i)
+    {
+        if(parent[i] == -1)
+            return i;
+
+        return parent[i] = DSU_find(parent[i]);
+    }
+
+    void DSU_union(int x, int y)
+    {
+        int X = DSU_find(x);
+        int Y = DSU_find(y);
+
+        if(rank[X] > rank[Y])
+            std::swap(X, Y);
+
+        parent[X] = Y;
+        rank[Y] += rank[X];
+    }
+};
