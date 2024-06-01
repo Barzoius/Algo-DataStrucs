d61a6087a4a583dec8b87c984dff43b7294fc404
Barzoius <95474661+Barzoius@users.noreply.github.com>
2024-05-31 17:46:30 -1100

    Update rayCast.cpp

--- Algorithms/Geometric_Algorithms/PointInsideConvexPoygon/RayCasting/rayCast.cpp
+++ Algorithms/Geometric_Algorithms/PointInsideConvexPoygon/RayCasting/rayCast.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 
 bool isPointOnSegment(std::pair<int, int> point, std::pair<int, int> segStart, std::pair<int, int> segEnd) {
     int xP = point.first, yP = point.second;
     int x1 = segStart.first, y1 = segStart.second;
     int x2 = segEnd.first, y2 = segEnd.second;
-
 
     int crossProduct = (y2 - y1) * (xP - x1) - (x2 - x1) * (yP - y1);
     if (crossProduct != 0) {
@@ -19,63 +19,56 @@ bool isPointOnSegment(std::pair<int, int> point, std::pair<int, int> segStart, s
     return true;
 }
 
-std::string position(std::vector<std::pair<std::pair<int, int>, std::pair<int, int>>>& edgeList,
-                     std::pair<int, int> point)
-{
+std::string position(const std::vector<std::pair<int, int>>& polyPointsList, std::pair<int, int> point) {
+    bool isInside = false;
     int cnt = 0;
-    for(const auto& edge : edgeList){
-        int xPE1 = edge.first.first;
-        int yPE1 = edge.first.second;
 
-        int xPE2 = edge.second.first;
-        int yPE2 = edge.second.second;
+    for (const auto& polyPoint : polyPointsList) {
+        int x1 = polyPoint.first, y1 = polyPoint.second;
+        const auto& nextPoint = (polyPoint == polyPointsList.back()) ? polyPointsList.front() : *(std::next(&polyPoint));
 
-        if (isPointOnSegment(point, {xPE1, yPE1}, {xPE2, yPE2})) {
+        int x2 = nextPoint.first, y2 = nextPoint.second;
+
+        if (isPointOnSegment(point, polyPoint, nextPoint)) {
             return "BOUNDARY";
         }
 
-        if( (point.second < yPE1) != (point.second < yPE2) &&
-             point.first < xPE1 + ((point.second - yPE1) / (yPE2 -yPE1)) * (xPE2 - xPE1) )
+        if (((y1 > point.second) != (y2 > point.second)) &&
+            (point.first < (double)(x2 - x1) * (point.second - y1) / (double)(y2 - y1) + x1)) {
+            isInside = !isInside;
             cnt++;
+        }
     }
 
-    if(cnt % 2 == 1)
-        return "INSIDE";
-    else
+    if (cnt % 2 == 0)
         return "OUTSIDE";
+    else
+        return "INSIDE";
 }
 
-int main(){
+int main() {
     std::vector<std::pair<int, int>> polyPointsList;
     int nr_polyPoints;
-    std::cin>>nr_polyPoints;
+    std::cin >> nr_polyPoints;
 
     int x, y;
-    for(int i = 0; i < nr_polyPoints; i++){
-        std::cin>>x>>y;
-        polyPointsList.emplace_back(std::make_pair(x, y));
-    }
-
-    std::vector<std::pair<std::pair<int, int>, std::pair<int, int>>> edgeList;
-    for(int i = 0; i < nr_polyPoints; i++) {
-        std::pair<int, int> start = polyPointsList[i];
-        std::pair<int, int> end = polyPointsList[(i + 1) % nr_polyPoints];
-        edgeList.emplace_back(start, end);
+    for (int i = 0; i < nr_polyPoints; i++) {
+        std::cin >> x >> y;
+        polyPointsList.emplace_back(x, y);
     }
 
     int nr_points;
-    std::cin>>nr_points;
+    std::cin >> nr_points;
     std::vector<std::pair<int, int>> pointsList;
-    for(int i = 0; i < nr_points; i++){
-        std::cin>>x>>y;
-        pointsList.emplace_back(std::make_pair(x, y));
+    for (int i = 0; i < nr_points; i++) {
+        std::cin >> x >> y;
+        pointsList.emplace_back(x, y);
     }
 
-    for(const auto& point : pointsList){
-        std::cout<<position(edgeList, point)<<"\n";
+    for (const auto& point : pointsList) {
+        std::cout << position(polyPointsList, point) << "\n";
     }
 
-    std::cout<<std::flush;
+    //std::cout << std::flush;
     return 0;
 }
-
